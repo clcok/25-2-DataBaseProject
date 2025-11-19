d8eb702c4eab1123e832fd3efcba9f395ff443e7
sinyeowon <ioohyou@knu.ac.kr>
2025-11-19 13:53:02 +0900

    feat: 사용자 아이디 검색 기능 추가

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,4 +1,51 @@
 package main.java.dao;
 
+import main.java.dto.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+
 public class UserDAO {
+
+    // 아이디 키워드 검색
+    public List<User> findByUserIdKeyword(String keyword) {
+        List<User> result = new ArrayList<>();
+
+        String sql = "SELECT Userid, Name, Password, Sex, Birth_date " +
+                "FROM USERS " +
+                "WHERE Userid LIKE '%' || ? || '%'";
+
+        try {
+            Connection conn = DBManager.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, keyword);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                User user = new User(
+                        rs.getString("Userid"),
+                        rs.getString("Name"),
+                        rs.getString("Password"),
+                        rs.getString("Sex"),
+                        rs.getDate("Birth_date")
+                );
+                result.add(user);
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
