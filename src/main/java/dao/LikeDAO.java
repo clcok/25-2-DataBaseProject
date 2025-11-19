1deae95da8952f16c4c4e69041a3828d2bc5cf44
sinyeowon <ioohyou@knu.ac.kr>
2025-11-19 14:08:01 +0900

    feat: LikeDAO에 유저별 받은 좋아요 수 조회 기능 추가

--- src/main/java/dao/LikeDAO.java
+++ src/main/java/dao/LikeDAO.java
@@ -1,4 +1,40 @@
 package main.java.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class LikeDAO {
+
+    // 특정 유저가 받은 좋아요 총 개수
+    public int countLikesReceived(String userId) {
+        String sql = """
+            SELECT COUNT(*) AS cnt
+            FROM POST P
+            JOIN LIKES L ON P.Post_id = L.Post_id
+            WHERE P.User_id = ?
+            """;
+
+        int cnt = 0;
+
+        try {
+            Connection conn = DBManager.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userId);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                cnt = rs.getInt("cnt");
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return cnt;
+    }
+
 }
